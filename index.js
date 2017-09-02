b44151cb95c3fb78b7b1e1856caaee4f76ef4394
玄悟 <zhifeng.xzf@alibaba-inc.com>
2017-09-02 12:02:29 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var https = require('https');
 https.createServer(function(req,res){
     res.writeHead(200,{'Content-Type': 'text/plain'});
     res.end(JSON.stringify({userName:'helloWorld'}));
-}).listen(3389);
+}).listen(6001);
 
-console.log('server is running on port 3389');
+console.log('server is running on port 6001');
