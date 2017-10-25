ebcc6fd507f2a1f5ad6e46e18c4e670e55967100
玄悟 <zhifeng.xzf@alibaba-inc.com>
2017-10-25 16:44:04 +0800

    update

--- app-douban.js
+++ app-douban.js
@@ -279,6 +279,7 @@ app.post('/pubdate', function(req, res){
         cinemaId: req.body.cinemaId,
         cinemaName: req.body.cinemaName,
         cinemaAddress: req.body.cinemaAddress,
+        words: req.body.words,
         status: 0, // 0未匹配 1匹配中 2匹配成功
         createTime: Date.now().toString()
     };
