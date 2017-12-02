9655fd263a7d2cee10528630a0fc5d09b2acc9be
玄悟 <zhifeng.xzf@alibaba-inc.com>
2017-12-03 01:04:15 +0800

    update

--- app-film.js
+++ app-film.js
@@ -6,8 +6,28 @@ var express = require('express');
 var app = express();
 var https = require('https');
 var fs = require('fs');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var OSS = require('ali-oss');
+var co = require('co');
+var client = new OSS({
+    region: 'oss-cn-hangzhou',
+    accessKeyId: 'LTAIrUHBoHLwlUNY',
+    accessKeySecret: 'OvuJdzBuziDOIQFRD4gbZXI1fDQ8qC',
+    bucket: 'yueavatar'
+});
+// 引入json解析中间件
+var bodyParser = require('body-parser');
+// 添加json解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-var baseUrl = 'https://movie.douban.com';
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var DB_CONN_STR = 'mongodb://localhost:27017/yue'; 
+
+var baseUrl = 'http://www.dbmeinv.com';
+var baseUrl1 = 'https://movie.douban.com';
 const successCode = 0, failCode = -1;
 
 function isEmpty(obj){
@@ -16,39 +36,138 @@ function isEmpty(obj){
     }
     return true;
 }
+function trim(str){  
+  return str.replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');  
+}
+
+app.get('/', function(req, res){
+    res.send('<h1>约吗？</h1>');
+});
 
 app.get('/nowplaying', function(req, res){
     var city = req.query.city;
     var route = '/cinema/nowplaying/' + city + '/';
     res.header("Content-Type", "application/json; charset=utf-8");
-    superagent.get(baseUrl+route)
+    superagent.get(baseUrl1+route)
     .charset('utf-8')
     .end(function (err, sres) {
-        var items = [];
         if (err) {
             console.log('ERR: ' + err);
-            res.json({code: failCode, msg: err, sets:items});
+            res.json({code: failCode, msg: err});
             return;
         }
         var $ = cheerio.load(sres.text);
+        var dataObj = {},
+            films = [],
+            districts = [];
         $('#nowplaying .lists .list-item').each(function (idx, element) {
             if(idx < 15) {
                 var $element = $(element),
                     $poster = $element.find('.poster img');
-                items.push({
+                films.push({
+                    id: $element.attr('id'),
                     img : $poster.attr('src'),
                     title : $element.data('title'),
-                    rate : $element.data('score')
+                    rate : $element.data('score'),
+                    release: $element.data('release'),
+                    duration: $element.data('duration'),
+                    region: $element.data('region'),
+                    director: $element.data('director'),
+                    actors: $element.data('actors')
                 });
             }
         });
-        res.json({code: successCode, msg: "", data:items});
+        dataObj.filmList = films;       
+        res.json({code: successCode, msg: "", data: dataObj});
+    });
+});
+
+app.get('/getopenid', function(req, res){
+    var code = req.query.code;
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    superagent.get('https://api.weixin.qq.com/sns/jscode2session?appid=wx288b9aa48204f09c&secret=7f0d2d16a6d82ddb3fd3ade56bc23712&js_code='+code+'&grant_type=authorization_code')
+    .charset('utf-8')
+    .end(function (err, sres) {
+        if (err) {
+            res.json({code: failCode, msg: err});
+            return;
+        }
+        var openId = JSON.parse(sres.text).openid;
+        res.json({code: successCode, msg: "", data: openId});        
+    });
+
+});
+
+app.get('/adduser', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    var userInfo = {
+        openId: req.query.openId,
+        nickName: req.query.nickName,
+        gender: Number(req.query.gender),
+        language: req.query.language,
+        city: req.query.city,
+        province: req.query.province,
+        country: req.query.country,
+        avatarUrl: req.query.avatarUrl
+    };
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        console.log("adduser连接成功！");
+        //执行插入数据操作
+        var collection = db.collection('user');
+        collection.find({"openId":userInfo.openId}).toArray(function(err, items){        
+            if(items.length>0) {
+                res.json({code: 1, msg: "", data: items[0]});
+
+                //关闭数据库
+                db.close();
+            } else {
+
+                //插入数据
+                collection.insert(userInfo, function(error, result) { 
+                    res.json({code: successCode, msg: "", data: result}); 
+                    db.close();
+                });
+            }
+        });
+    });
+});
+app.post('/broadcast', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    var userId = req.body.userId;
+    var dateInfo = {
+        userId: userId,
+        avatarUrl: req.body.avatarUrl,
+        nickName: req.body.nickName,
+        filmId: req.body.filmId+'',
+        filmName: req.body.filmName,
+        words: req.body.words,
+        status: 1, // 0未匹配 1匹配中 2匹配成功
+        createTime: Date.now()
+    };
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        console.log("db连接成功！");
+        var collection = db.collection('vote');
+        collection.insert(dateInfo, function(err, result) { 
+            //如果存在错误
+            if(err) {
+                console.log('Error:'+ err);
+                res.json({code: failCode, data: err}); 
+                db.close();
+                return;
+            } 
+            res.json({code: successCode, msg: "", data: result}); 
+
+            collection.update({userId:userId,status:1,_id:{$ne:ObjectID(result.insertedIds[0])}},{$set:{status:0}}, function(err1, result1) { 
+                db.close();
+            });
+        });
     });
 });
 
 var options = {
-	key: fs.readFileSync('./keys/214248838510598.key'),
-	cert: fs.readFileSync('./keys/214248838510598.pem')
+    key: fs.readFileSync('./keys/214248838510598.key'),
+    cert: fs.readFileSync('./keys/214248838510598.pem')
 };
 https.createServer(options, app).listen(3000, function(req, res){
     console.log('server is running on port 3000');
