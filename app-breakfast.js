2b1aa1ec2e8a79f16c7efd26fb70a2044b547d24
玄悟 <zhifeng.xzf@alibaba-inc.com>
2017-12-14 11:03:49 +0800

    update

---
+++ app-breakfast.js
@@ -0,0 +1,234 @@
+var superagent = require('superagent');
+var charset = require('superagent-charset');
+charset(superagent);
+var cheerio = require('cheerio');
+var express = require('express');
+var app = express();
+var https = require('https');
+var request = require('request'); 
+var fs = require('fs');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var OSS = require('ali-oss');
+var co = require('co');
+var client = new OSS({
+    region: 'oss-cn-hangzhou',
+    accessKeyId: 'LTAIrUHBoHLwlUNY',
+    accessKeySecret: 'OvuJdzBuziDOIQFRD4gbZXI1fDQ8qC',
+    bucket: 'breakfastcover'
+});
+// 引入json解析中间件
+var bodyParser = require('body-parser');
+// 添加json解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var DB_CONN_STR = 'mongodb://localhost:27017/yue'; 
+
+var baseUrl = 'https://movie.douban.com';
+const successCode = 0, failCode = -1;
+
+function isEmpty(obj){
+    for (var i in obj){
+        return false;
+    }
+    return true;
+}
+function trim(str){  
+  return str.replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');  
+}
+
+app.get('/breakfast', function(req, res){
+    res.send('<h1>天天晒早餐</h1>');
+});
+
+app.get('/fetchopenid', function(req, res){
+    var code = req.query.code;
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    superagent.get('https://api.weixin.qq.com/sns/jscode2session?appid=wx288b9aa48204f09c&secret=7f0d2d16a6d82ddb3fd3ade56bc23712&js_code='+code+'&grant_type=authorization_code')
+    .charset('utf-8')
+    .end(function (err, sres) {
+        if (err) {
+            res.json({code: failCode, msg: err});
+            return;
+        }
+        var openId = JSON.parse(sres.text).openid;
+        res.json({code: successCode, msg: "", data: openId});        
+    });
+
+});
+
+app.get('/fetchaccesstoken', function(req, res){
+    var code = req.query.code;
+    res.header("Content-Type", "application/json; charset=utf-8");
+       
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('wx');
+        var requestNewToken = function(){
+            superagent.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx288b9aa48204f09c&secret=7f0d2d16a6d82ddb3fd3ade56bc23712')
+            .charset('utf-8')
+            .end(function (err, sres) {
+                if (err) {
+                    res.json({code: failCode, msg: err});
+                    return;
+                }
+                var dataJson = JSON.parse(sres.text),
+                    access_token = dataJson.access_token,
+                    expires_in = dataJson.expires_in;
+                collection.update({name:'token'},{$set:{
+                    access_token: access_token,
+                    expires_time: Date.now() + expires_in*1000
+                }}, function(err, result) { 
+                    res.json({code: successCode, msg: "", data: access_token});  
+                    db.close();
+                });
+                      
+            });
+        };
+        collection.find({name:'token'}).toArray(function(err, items){ 
+            if(items.length>0) {
+                var now = Date.now();
+                if(now < items[0].expires_time) {
+                    res.json({code: successCode, msg: "", data: items[0].access_token}); 
+                    db.close();
+                } else {
+                    requestNewToken();
+                }
+            } else {
+                requestNewToken();
+            }
+        });
+    });
+});
+app.get('/fetchqrcode', function(req, res){
+    var accessToken = req.query.accessToken,
+        scene = req.query.scene,
+        id = scene.split('=')[1],
+        path = req.query.path,
+        width = Number(req.query.width);
+    res.header("Content-Type", "application/json; charset=utf-8");
+    var filePath = './uploads/qrcode/'+id+'.png';
+    request({ 
+        method: 'POST', 
+        url: 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=' + accessToken, 
+        body: JSON.stringify({scene:scene,path:path,width:width}) 
+    }).pipe(fs.createWriteStream(filePath))
+    .on('close', function() {
+        co(function* () {
+            var stream = fs.createReadStream(filePath);
+            var result = yield client1.putStream(id+'.png', stream);
+            res.json({code: successCode, msg: "", data: result.url.replace(/http:/,'https:')});
+            fs.unlinkSync(filePath);
+        });
+    });
+});
+
+app.get('/createuser', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    var userInfo = {
+        openId: req.query.openId,
+        nickName: req.query.nickName,
+        gender: Number(req.query.gender),
+        language: req.query.language,
+        city: req.query.city,
+        province: req.query.province,
+        country: req.query.country,
+        avatarUrl: req.query.avatarUrl
+    };
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        console.log("adduser连接成功！");
+        //执行插入数据操作
+        var collection = db.collection('user');
+        collection.find({"openId":userInfo.openId}).toArray(function(err, items){        
+            if(items.length>0) {
+                res.json({code: 1, msg: "", data: items[0]});
+
+                //关闭数据库
+                db.close();
+            } else {
+
+                //插入数据
+                collection.insert(userInfo, function(error, result) { 
+                    res.json({code: successCode, msg: "", data: result}); 
+                    db.close();
+                });
+            }
+        });
+    });
+});
+
+app.post('/uploadcover', upload.single('file'), function (req, res, next) {
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    // 文件路径
+    var filePath = './' + req.file.path;  
+    // 文件类型
+    var fileType = req.file.mimetype;
+    var lastName = '';
+    switch (fileType){
+        case 'image/png':
+            lastName = '.png';
+            break;
+        case 'image/jpeg':
+            lastName = '.jpg';
+            break;
+        default:
+            lastName = '.jpg';
+            break;
+    }
+    var userId = req.body.userId;
+    // 构建图片名
+    var fileName = userId + '_' + Date.now() + lastName;
+
+    co(function* () {
+        var result = yield client.put(fileName, filePath);
+
+        // 上传之后删除本地文件
+        fs.unlinkSync(filePath);
+
+        res.json({code: successCode, msg: "", data: result.url.replace(/http:/,'https:')});  
+        db.close();
+    }).catch(function (err) {
+        console.log(err);
+    }); 
+})
+
+app.post('/pubbreakfast', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    var userId = req.body.userId;
+    var pubInfo = {
+        userId: userId,
+        avatarUrl: req.body.avatarUrl,
+        nickName: req.body.nickName,
+        
+        words: req.body.words,
+        createTime: Date.now()
+    };
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        console.log("breakfast连接成功！");
+        var collection = db.collection('breakfast');
+        collection.insert(pubInfo, function(err, result) { 
+            //如果存在错误
+            if(err) {
+                console.log('Error:'+ err);
+                res.json({code: failCode, data: err}); 
+                db.close();
+                return;
+            } 
+            res.json({code: successCode, msg: "", data: result}); 
+            db.close();
+        });
+    });
+});
+
+
+var options = {
+    key: fs.readFileSync('./keys/214248838510598.key'),
+    cert: fs.readFileSync('./keys/214248838510598.pem')
+};
+https.createServer(options, app).listen(3000, function(req, res){
+    console.log('server is running on port 3000');
+});
