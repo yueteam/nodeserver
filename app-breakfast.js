6d9c72343605d5ea04c27800688f533ad981ee03
玄悟 <zhifeng.xzf@alibaba-inc.com>
2017-12-27 14:00:14 +0800

    update

--- app-breakfast.js
+++ app-breakfast.js
@@ -27,12 +27,8 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var DB_CONN_STR = 'mongodb://localhost:27017/breakfast'; 
 
-var baseUrl = 'https://movie.douban.com';
 const successCode = 0, failCode = -1;
-var breakfastWXInfo = {
-        appid: 'wx8f2f018d92b566a6',
-        secret: 'dcc283dfa0ce262dd03d410fe4c04d00'
-    };
+
 function isEmpty(obj){
     for (var i in obj){
         return false;
@@ -43,11 +39,22 @@ function trim(str){
   return str.replace(/^(\s|\u00A0)+/,'').replace(/(\s|\u00A0)+$/,'');  
 }
 
+/**
+ * [breakfast] 天天晒早餐
+ * @type {Object}
+ */
+app.get('/tGFrYeQFZG.txt', function(req, res){
+    res.send('5dc88363851a7957e9616c47004dc67e');
+});
+var breakfastWXInfo = {
+        appid: 'wx2992e5dce30736a9',
+        secret: 'b2befe7883f36ddc7808c998b27158a0'
+    };
 app.get('/breakfast', function(req, res){
     res.send('<h1>天天晒早餐</h1>');
 });
 
-app.get('/fetchopenid', function(req, res){
+app.get('/bfopenid', function(req, res){
     var code = req.query.code;
     res.header("Content-Type", "application/json; charset=utf-8");
 
@@ -64,7 +71,7 @@ app.get('/fetchopenid', function(req, res){
 
 });
 
-app.get('/fetchaccesstoken', function(req, res){
+app.get('/bfaccesstoken', function(req, res){
     var code = req.query.code;
     res.header("Content-Type", "application/json; charset=utf-8");
        
@@ -106,7 +113,7 @@ app.get('/fetchaccesstoken', function(req, res){
         });
     });
 });
-app.get('/fetchqrcode', function(req, res){
+app.get('/bfqrcode', function(req, res){
     var accessToken = req.query.accessToken,
         scene = req.query.scene,
         id = scene.split('=')[1],
@@ -163,6 +170,29 @@ app.get('/addbfuser', function(req, res){
     });
 });
 
+app.get('/findbfuser', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    var userId = req.query.id;
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        console.log("findbfuser连接成功！");
+        var collection = db.collection('user');
+        var collection_meal = db.collection('meal');
+        collection.findOne({_id: ObjectID(userId)}, function(err1, item){  
+            if(err1) {
+                res.json({code: failCode, data: err1}); 
+                db.close();
+                return;
+            }      
+            collection_meal.aggregate([{$match:{userId:userId}},{$group:{_id:"$userId", pub_num:{$sum:1}, forked_num:{$sum:"$forkCount"}}}], function(err2, result) {                     
+                collection_meal.aggregate([{$match:{fork_users:ObjectID(userId)}},{$group:{_id:1, fork_num:{$sum:1}}}], function(err3, result1) {                        
+                    res.json({code: successCode, msg: "", data: item, count: result, count1: result1});
+                    db.close();
+                });
+            });
+        });
+    });
+});
+
 app.post('/uploadcover', upload.single('file'), function (req, res, next) {
     res.header("Content-Type", "application/json; charset=utf-8");
 
@@ -192,28 +222,43 @@ app.post('/uploadcover', upload.single('file'), function (req, res, next) {
         // 上传之后删除本地文件
         fs.unlinkSync(filePath);
 
-        res.json({code: successCode, msg: "", data: result.url.replace(/http:/,'https:')});  
+        res.send(result.url.replace(/http:/,'https:'));  
         db.close();
     }).catch(function (err) {
         console.log(err);
     }); 
 })
 
-app.post('/pubbreakfast', function(req, res){
+app.post('/addmeal', function(req, res){
     res.header("Content-Type", "application/json; charset=utf-8");
     var userId = req.body.userId;
-    var pubInfo = {
+    var now = Date.now(),
+        nowdate = new Date(),
+        year = nowdate.getFullYear(),
+        month = nowdate.getMonth()+1,
+        date = nowdate.getDate(),
+        dayStr = year+'/'+month+'/'+date;
+
+    var mealInfo = {
         userId: userId,
         avatarUrl: req.body.avatarUrl,
         nickName: req.body.nickName,
-        
-        words: req.body.words,
-        createTime: Date.now()
+        coverImg: req.body.coverImg,
+        title: req.body.title,
+        desc: req.body.desc,
+        cookTime: Number(req.body.cookTime),
+        forkCount: 0,
+        fork_users: [],
+        day: dayStr,
+        createTime: now
     };
+    if(userId === '5a40efbcf18ac53540f49459') {
+        mealInfo.official = true;
+    }
     MongoClient.connect(DB_CONN_STR, function(err, db) {
-        console.log("breakfast连接成功！");
-        var collection = db.collection('breakfast');
-        collection.insert(pubInfo, function(err, result) { 
+        console.log("meal连接成功！");
+        var collection = db.collection('meal');
+        collection.insert(mealInfo, function(err, result) { 
             //如果存在错误
             if(err) {
                 console.log('Error:'+ err);
@@ -221,12 +266,172 @@ app.post('/pubbreakfast', function(req, res){
                 db.close();
                 return;
             } 
-            res.json({code: successCode, msg: "", data: result}); 
+            res.json({code: successCode, msg: "", data: result.insertedIds[0]}); 
             db.close();
         });
     });
 });
 
+function inArray(search, arr) {
+    var isExist = 0;
+    arr.forEach(function(item){
+        if(item.toString() === search){
+            isExist = 1;
+            return isExist;
+        }
+    });
+    return isExist;
+}
+app.get('/getmeal', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    var pageNo = parseInt(req.query.pageNo);
+    var userId = req.query.userId;
+    var skipCount = (pageNo-1)*30;
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('meal');
+        collection.find().sort({'createTime':-1}).limit(30).skip(skipCount).toArray(function(err, items){        
+            if(items.length>0) {
+                var list = [];
+                items.forEach(function(item){
+                    var newItem = {
+                        _id: item._id,
+                        userId: item.userId,
+                        avatarUrl: item.avatarUrl,
+                        coverImg: item.coverImg,
+                        title: item.title,
+                        desc: item.desc,
+                        forkCount: item.forkCount,
+                        day: item.day,
+                        official: item.official || false
+                    }
+                    var forkUsers = item.fork_users;
+                    if(inArray(userId,forkUsers) === 1) {
+                        newItem.forked = true;
+                    }
+                    list.push(newItem);
+                });
+                res.json({code: successCode, msg: "", data: list});
+            } else {
+                res.json({code: failCode, msg: "没有更多了~"});
+            }
+            //关闭数据库
+            db.close();
+        });
+    });
+});
+
+app.get('/fork', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    var userId = req.query.userId,
+        mealId = req.query.mealId;
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('meal');
+        collection.update({_id: ObjectID(mealId)}, {$inc: {forkCount: 1}, $addToSet: {fork_users: ObjectID(userId)}}, function(err1, result) {  
+            if(err1) {
+                res.json({code: failCode, data: err1}); 
+                db.close();
+                return;
+            } 
+            res.json({code: successCode, msg: ""});
+            db.close();
+        });
+    });
+});
+app.get('/unfork', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    var userId = req.query.userId,
+        mealId = req.query.mealId;
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('meal');
+        collection.update({_id: ObjectID(mealId)}, {$inc: {forkCount: -1}, $pull: {fork_users: ObjectID(userId)}}, function(err1, result) {  
+            if(err1) {
+                res.json({code: failCode, data: err1}); 
+                db.close();
+                return;
+            } 
+            res.json({code: successCode, msg: ""});
+            db.close();
+        });
+    });
+});
+
+app.get('/mealdetail', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    var id = req.query.id;
+    var userId = req.query.userId;
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('meal');
+        var collection_user = db.collection('user');
+        collection.findOne({_id: ObjectID(id)}, function(err1, item){        
+            if(err1) {
+                res.json({code: failCode, data: err1}); 
+                db.close();
+                return;
+            } 
+
+            var newItem = {
+                _id: item._id,
+                userId: item.userId,
+                nickName: item.nickName,
+                avatarUrl: item.avatarUrl,
+                coverImg: item.coverImg,
+                title: item.title,
+                desc: item.desc,
+                cookTime: item.cookTime,
+                forkCount: item.forkCount,
+                day: item.day,
+                official: item.official || false,
+                materials: item.materials || [],
+                steps: item.steps || []
+            }
+            if(inArray(userId, item.fork_users) === 1) {
+                newItem.forked = true;
+            }
+            res.json({code: successCode, msg: "", data: newItem});
+            db.close();
+        });
+    });
+});
+app.post('/editmeal', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    var mealId = req.body.id;
+    var materials = JSON.parse(req.body.materials);
+    var steps = JSON.parse(req.body.steps);
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('meal');
+        collection.update({_id: ObjectID(mealId)}, {$set:{materials:materials,steps:steps}}, function(err1, result) {  
+            if(err1) {
+                res.json({code: failCode, data: err1}); 
+                db.close();
+                return;
+            } 
+            res.json({code: successCode, msg: ""});
+            db.close();
+        });
+    });
+});
+app.get('/mymeal', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    var userId = req.query.userId;
+    MongoClient.connect(DB_CONN_STR, function(err, db) {
+        var collection = db.collection('meal');
+        collection.find({userId: userId}, {title:1,coverImg:1,forkCount:1}).sort({'createTime':-1}).limit(30).toArray(function(err, items){        
+            if(items.length>0) {
+                res.json({code: successCode, msg: "", data: items});
+            } else {
+                res.json({code: failCode, msg: "没有"});
+            }
+            //关闭数据库
+            db.close();
+        });
+    });
+});
 
 var options = {
     key: fs.readFileSync('./keys/214248838510598.key'),
