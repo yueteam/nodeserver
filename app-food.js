f30c86b8012165575cf7f33e4612dae699a494ac
玄悟 <zhifeng.xzf@alibaba-inc.com>
2018-02-01 11:43:36 +0800

    update

--- app-food.js
+++ app-food.js
@@ -68,6 +68,67 @@ function inArray(search, arr) {
     return isExist;
 }
 
+var baseUrl = 'http://www.dbmeinv.com';
+app.get('/tags', function(req, res){
+    res.header("Content-Type", "application/json; charset=utf-8");
+    superagent.get(baseUrl)
+    .charset('utf-8')
+    .end(function (err, sres) {
+        var items = [];
+        if (err) {
+            console.log('ERR: ' + err);
+            res.json({code: failCode, msg: err, sets:items});
+            return;
+        }
+        var $ = cheerio.load(sres.text);
+        $('#main .panel-heading ul.nav li a').each(function (idx, element) {
+            var $element = $(element);
+            var hrefStr = $element.attr('href');
+            var cid = hrefStr.match(/cid=(\d)/);
+            cid = isEmpty(cid) ? "0" : cid[1];
+            items.push({
+                title : $element.text(),
+                href : hrefStr,
+                cid : cid,
+            });
+        });
+        res.json({code: successCode, msg: "", data:items});
+    });
+});
+
+app.get('/girls', function(req, res){
+    var cid = req.query.c;
+    var page = req.query.p;
+    cid = !isEmpty(cid) ? cid : '0';
+    page = !isEmpty(page) ? page : '1';
+    var route = '/dbgroup/show.htm?cid=' + cid + '&pager_offset=' + page;
+    res.header("Content-Type", "application/json; charset=utf-8");
+    superagent.get(baseUrl+route)
+    .charset('utf-8')
+    .end(function (err, sres) {
+        if (err) {
+            console.log('ERR: ' + err);
+            return next(err);
+        }
+        var $ = cheerio.load(sres.text);
+        var items = [];
+        var t1 = new Date().getTime();
+        $('#main .panel-body ul.thumbnails li.span3 .img_single a').each(function (idx, element) {
+            var $element = $(element);
+            var $subElement = $element.find('img.height_min');
+            var thumbImgSrc = $subElement.attr('src');
+            items.push({
+                title : $subElement.attr('title'),
+                href : $element.attr('href'),
+                largeSrc : isEmpty(thumbImgSrc) ? "" : thumbImgSrc.replace('bmiddle', 'large'),
+                thumbSrc : thumbImgSrc,
+                smallSrc : isEmpty(thumbImgSrc) ? "" : thumbImgSrc.replace('bmiddle', 'small'),
+            });
+        });
+        res.json({code: successCode, msg: "", data:items});
+    });
+});
+
 /**
  * [breakfast] 健康知食
  * @type {Object}
