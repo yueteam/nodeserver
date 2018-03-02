fa888fcb8bae5951d77be0f4cb81b8c9b03d8879
玄悟 <zhifeng.xzf@alibaba-inc.com>
2018-03-02 19:21:40 +0800

    update

--- app-food.js
+++ app-food.js
@@ -237,9 +237,10 @@ app.get('/getweatherinfo', function(req, res){
             if(item) {
                 var lastUpdateTime = item.updateTime;
                 var nowTimeNum = parseInt(hour) + parseInt(minute)*0.01;
-                console.log(nowTimeNum);
                 if((lastUpdateTime === '07:30' && nowTimeNum < 11.3) || (lastUpdateTime === '11:30' && nowTimeNum < 18) || lastUpdateTime === '18:00') {
                     updated = true;
+                } else {
+                    collection.remove({city: city, date: dateStr});
                 }
             }
 
